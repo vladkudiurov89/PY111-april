6652f4cc09894ef4e9efa9488b95e7a1e42b6438
vlakud <v>
2019-04-24 21:14:52 +0300

    HI b0

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -6,34 +6,44 @@ This module implements some functions based on linear search algo
 :param arr: Array containing numbers
 :return: index of first occurrence of minimal element in array"""
 
-arr = [1, 3, 4, 6, 2, 9, 11, 18]
+arr = [17, 4, 1, 3, 4, 6, 2, 9, 11, 18]
+
+
+# def min_search(arr) -> int:
+# 	v = arr[0]
+# 	for i in arr:
+# 		if v < i:
+# 			pass
+# 		else:
+# 			v = i
+# 	return arr.index(v)
 
 
 def min_search(arr) -> int:
-	v = arr[0]
-	for i in arr:
-		if v < i:
-			pass
-		else:
-			v = i
+	m = arr[0]
+	n = 0
+	for i in range(len(arr)):
+		if arr[i] < m:
+			m = arr[i]
+			n = i
+	return n
 
-	return arr.index(v)
+# def min_search(arr) -> int:
+# 	m = arr[0]
+# 	n = 0
+# 	for i in arr:
+# 		if i < m:
+# 			m = arr[i]
+# 			n = [i]
+# 	return n
 
 
-min_search()
 
 
 
-def min_weight_search(Graph) -> tuple:
-	"""
-	Function that find edge in graph with minimal weight of all
 
-	:param Graph: NetworkX Graph (or digraph) with weighted edges
-	:return: tuple of nodes (node, node) the weight of edge between which is minimal (any occurrence)
-	"""
 
-	return None, None
 
-if __name__ == '__main__':
-    min_search(5)
+
+
 
